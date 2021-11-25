4b4ba05c60bf960d2af9f75246be5f94fc1f6683
AhsanRiaz9 <bcsf18m009@pucit.edu.pk>
2021-11-26 01:00:28 +0500

    Added all layout ui

---
+++ app/src/main/java/com/example/mc_revision_all_concepts/LayoutPractice.java
@@ -0,0 +1,14 @@
+package com.example.mc_revision_all_concepts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutPractice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_practice);
+    }
+}
--- app/src/main/java/com/example/mc_revision_all_concepts/MainActivity.java
+++ app/src/main/java/com/example/mc_revision_all_concepts/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.mc_revision_all_concepts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button layoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layoutBtn = findViewById(R.id.layoutBtn);
+        layoutBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.layoutBtn:
+            {
+                Intent intent = new Intent(MainActivity.this,LayoutPractice.class);
+                startActivity(intent);
+            }
+            break;
+        }
     }
 }
