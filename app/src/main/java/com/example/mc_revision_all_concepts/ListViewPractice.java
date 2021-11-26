659ce564751dd2c4c2833fca8639758b54aeec9c
AhsanRiaz9 <bcsf18m009@pucit.edu.pk>
2021-11-26 12:26:35 +0500

    Added alert code

--- app/src/main/java/com/example/mc_revision_all_concepts/ListViewPractice.java
+++ app/src/main/java/com/example/mc_revision_all_concepts/ListViewPractice.java
@@ -1,9 +1,11 @@
 package com.example.mc_revision_all_concepts;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +18,7 @@ import java.util.ArrayList;
 public class ListViewPractice extends AppCompatActivity implements View.OnClickListener {
 
     EditText editTextName;
-    Button addNameBtn;
+    Button addNameBtn,dilougeAlertBtn;
     ListView listView;
     ArrayList<String> namesList;
     ArrayAdapter<String> adapter;
@@ -26,7 +28,9 @@ public class ListViewPractice extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_list_view_practice);
         editTextName = findViewById(R.id.editTextName);
         addNameBtn = findViewById(R.id.addNameBtn);
+        dilougeAlertBtn = findViewById(R.id.dilougeAlertBtn);
         addNameBtn.setOnClickListener(this);
+        dilougeAlertBtn.setOnClickListener(this);
         listView = findViewById(R.id.listView);
         namesList = new ArrayList<String>();
         namesList.add("Ahsan");
@@ -35,6 +39,14 @@ public class ListViewPractice extends AppCompatActivity implements View.OnClickL
         namesList.add("Bilal");
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,namesList);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String msg = namesList.get(position) + " is selected";
+                // context is listViewPractice.this
+                Toast.makeText(ListViewPractice.this,msg,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -54,6 +66,16 @@ public class ListViewPractice extends AppCompatActivity implements View.OnClickL
                     Toast toast = Toast.makeText(getApplicationContext(),"Name can't be empty", Toast.LENGTH_LONG);
                     toast.show();
                 }
+            }
+            break;
+            case R.id.dilougeAlertBtn:
+            {
+                AlertDialog.Builder builder = builder = new AlertDialog.Builder(ListViewPractice.this);
+                builder.setMessage("Hi").setPositiveButton("yes",(dialog, which) -> {
+                   finish();
+                }).setNegativeButton("no",(dialog, which) -> {
+                    Toast.makeText(ListViewPractice.this, "Hi", Toast.LENGTH_SHORT).show();
+                }).setTitle("Alert Dilouge").show();
             }
             break;
         }
